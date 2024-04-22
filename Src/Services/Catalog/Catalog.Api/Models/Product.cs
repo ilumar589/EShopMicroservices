9bc7032abb1debf88cb3e64cd241102e8d1f2aaf
Eduard Parvu <eduard.parvu@rinftech.com>
2024-04-22 21:14:44 +0300

    -

---
+++ Src/Services/Catalog/Catalog.Api/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.Api.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public List<string> Category { get; set; } = Enumerable.Empty<string>().ToList();
+    public string Description { get; set; } = string.Empty;
+    public string ImageFile { get; set; } = string.Empty;
+    public decimal Price { get; set; } = 0;
+}
---
+++ Src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Immutable;
+using BuildingBlocks.CQRS;
+
+namespace Catalog.Api.Products.CreateProduct;
+
+public readonly record struct CreateProductCommand() : ICommand<CreateProductResult>
+{
+    public string Name { get; init; } = string.Empty;
+    public IReadOnlyList<string> Category { get; init; } = ImmutableList<string>.Empty;
+    public string Description { get; init; } = string.Empty;
+    public string ImageFile { get; init; } = string.Empty;
+    public decimal Price { get; init; } = 0;
+}
+
+public readonly record struct CreateProductResult()
+{
+    public Guid Id { get; init; } = Guid.Empty;
+}
+
+internal sealed class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
